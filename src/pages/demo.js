4d8f239f486393e07e119fe9870c79d96635a7fc
Samuel-Moo <samuelmoo05@gmail.com>
2023-06-03 22:56:31 -0600

    adaas

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -5,6 +5,8 @@ import toast, {Toaster} from "react-hot-toast"
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { Connection, SystemProgram, Transaction, PublicKey, LAMPORTS_PER_SOL, clusterApiUrl, SendTransactionError } from '@solana/web3.js'
+import Navbar from '@/components/Navbar'
+ 
 
 const SOLANA_NETWORK = "devnet";
 
@@ -147,8 +149,9 @@ export default function MyComponent() {
   }
   return (
 <>
-<div className='grid grid-cols-2'>
-<div class=" p-6 my-10 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<Navbar/>
+<div className='grid grid-cols-3 bg-slate-900'>
+<div class="col-span-2 p-6  bg-white border border-gray-200  shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
     </a>
@@ -157,6 +160,14 @@ export default function MyComponent() {
         Donate Now
         <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
     </a>
+</div>
+<div class='col-start-3'>
+  <Image 
+  src={require('../../public/ballenas.jpg')}
+  width={1000}
+  height={1000}
+  alt='ballena'
+  />
 </div>
 </div>  
 </>
