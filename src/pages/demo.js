913e83fd5a5efe6df7009b96de6f5c3b3a0408e3
Samuel-Moo <samuelmoo05@gmail.com>
2023-06-03 22:13:30 -0600

    Card

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -146,40 +146,19 @@ export default function MyComponent() {
     }
   }
   return (
-    <>
-      <h1>
-        Conecta el wallet
-      </h1>
-
-        {publicKey ?(
-
-          <div>
-            <h1>Tu numero de wallet es {publicKey}</h1>
-            <br />
-            <h1>Tienes {balance} SOL</h1>
-            <br />
-
-            <h1>Enviar a</h1>
-            <input type='text' className='border-4 border-slate-950' onChange={handleReciverChange} />
-
-            <br />
-
-            <h1>Cuanto</h1>
-            <input type='text' className='border-4 border-slate-950' onChange={handleAmmountChange} />
-            <br />
-            <button className='bg-blue-500' onClick={() => {handleSubmit()}}>Mandar feria</button>
-            <br />
-            <button className='bg-blue-500' onClick={() => {signOut()}}>Desconectar wallet</button>
-          </div>
-
-        ):(
-
-        
-
-      <div>
-        <button className='bg-blue-500' onClick={() => {signIn()}}>Conectar wallet</button>
-      </div>
-      ) }
-    </>
+<>
+<div className='grid grid-cols-2'>
+<div class=" p-6 my-10 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Donate Now
+        <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+    </a>
+</div>
+</div>  
+</>
   )
 }
